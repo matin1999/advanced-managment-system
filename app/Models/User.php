4e6ac0bf90f2c9c958e8779df3a4148f74a3404a
matin <matin.akhgari@ramzinex.com>
2023-08-30 19:32:38 +0330

    create models and relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,19 +51,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Article::class, 'user_id', 'id');
     }
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
